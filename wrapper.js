74836e9a8256360a9f1b2fdf3c1af6c954f1bf6c
Sascha van Gemmert <sascha.vangemmert@spilgames.com>
2013-03-16 20:13:28 +0100

    NEW added option to use config and tag download of jslint
    now you can also specify --json-config=YOUR-conf.json as a
    param to the commandline.
    The config is a json file specifying the options for JSLINT.

--- wrapper.js
+++ wrapper.js
@@ -15,38 +15,42 @@ var LINT = {
 	source: {
 		scheme: "https",
 		host: "raw.github.com",
-		path: "/douglascrockford/JSLint/master/jslint.js"
+        vsn : "1.0.0",
+        filename : "jslint.js",
+		path: "/spil-sascha/JSLint/"
 	}
 };
 
 var getJSLint, formatOutput, transformWarning, parseOptions, exit;
 
-var main = function(args) {
+var main = function (args) {
 	args = args.slice(2); // ignore Node command and script file
 	var opts = parseOptions(args);
 	var anon = opts.anon;
 	opts = opts.opts;
 
-	if(opts.jshint) {
+	if (opts.jshint) {
 		delete opts.jshint;
 		LINT.name = "JSHint";
 		LINT.filename = "jshint.js";
 		LINT.root = "JSHINT";
-		LINT.source.path = "/jshint/jshint/master/jshint.js";
+		LINT.source.vsn = "master";
+		LINT.source.filename = "jshint.js";
+		LINT.source.path = "/jshint/jshint/";
 	}
-	LINT.path = path.join(__dirname, LINT.filename);
+	LINT.path = path.join(__dirname, LINT.source.vsn + "-" + LINT.filename);
 
 	var verbose = false;
-	if(opts.verbose) {
+	if (opts.verbose) {
 		delete opts.verbose;
 		verbose = true;
 	}
-	if(opts.help || args.length === 0) {
+	if (opts.help || args.length === 0) {
 		var readme = fs.readFileSync(path.join(__dirname, "README"), "utf-8");
 		exit(args.length > 0, readme);
 	}
-	if(opts.upgrade) {
-		getJSLint(function(contents) {
+	if (opts.upgrade) {
+		getJSLint(function (contents) {
 			fs.writeFileSync(LINT.path, contents);
 			main([null, null, "--version"]); // XXX: hacky!?
 			exit(true);
@@ -57,23 +61,23 @@ var main = function(args) {
 	var jslint;
 	try {
 		jslint = fs.readFileSync(LINT.path, "utf-8");
-	} catch(exc) {
+	} catch (exc) {
 		exit(false, "ERROR: " + LINT.path + " not found - " +
 				"use `--upgrade` to initialize");
 	}
 
-	if(opts.version) {
+	if (opts.version) {
 		var sandbox = {};
 		vm.runInNewContext(jslint, sandbox);
 		exit(true, "JSLint Reporter v" + VERSION + "\n" +
 				LINT.name + " v" + sandbox[LINT.root].edition);
 	}
 
-	if(verbose) {
+	if (verbose) {
 		process.stderr.write("JSLint options: " + util.inspect(opts) + "\n");
 	}
 
-	var doLint = function(filepath) {
+	var doLint = function (filepath) {
 		var src = fs.readFileSync(filepath, "utf-8");
 		var sandbox = {
 			SRC: src,
@@ -83,10 +87,10 @@ var main = function(args) {
 		vm.runInNewContext(jslint + "\n" + code, sandbox);
 
 		var data = sandbox.data;
-		var implied = (data.implieds || []).map(function(item) {
+		var implied = (data.implieds || []).map(function (item) {
 			return transformWarning(item, "implied global");
 		});
-		var unused = (data.unused || []).map(function(item) {
+		var unused = (data.unused || []).map(function (item) {
 			return transformWarning(item, "unused variable");
 		});
 		return (data.errors || []).concat(implied).concat(unused);
@@ -94,7 +98,7 @@ var main = function(args) {
 
 	var errors = [];
 	var i;
-	for(i = 0; i < anon.length; i += 1) {
+	for (i = 0; i < anon.length; i += 1) {
 		var filepath = anon[i];
 		var err = doLint(filepath);
 		err = formatOutput(err, filepath);
@@ -102,9 +106,9 @@ var main = function(args) {
 	}
 	var pass = errors.length === 0;
 
-	if(!pass) {
+	if (!pass) {
 		util.print(errors.join("\n") + "\n");
-		if(verbose) {
+		if (verbose) {
 			process.stderr.write(String(errors.length) + " errors\n");
 		}
 	}
@@ -112,43 +116,51 @@ var main = function(args) {
 	exit(pass);
 };
 
-getJSLint = function(callback) {
+getJSLint = function (callback) {
 	var https = require(LINT.source.scheme);
 	var options = {
 		host: LINT.source.host,
-		path: LINT.source.path
+		path: LINT.source.path + LINT.source.vsn + "/" + LINT.source.filename
 	};
-	https.get(options, function(response) {
-		if(response.statusCode !== 200) {
+    util.print(JSON.stringify(options));
+	https.get(options, function (response) {
+		if (response.statusCode !== 200) {
 			exit(false, "failed to retrieve JSLint file");
 		}
 		response.setEncoding("utf8");
 		var body = [];
-		response.on("data", function(chunk) {
+		response.on("data", function (chunk) {
 			body.push(chunk);
 		});
-		response.on("end", function() {
+		response.on("end", function () {
 			callback(body.join(""));
 		});
 	});
 };
 
-formatOutput = function(errors, filepath) {
-	var lines = [];
-	var i;
-	for(i = 0; i < errors.length; i += 1) {
-		var error = errors[i];
-		if(error) { // last item might be null (if linting was aborted)
-			var line = [filepath, error.line, error.character, error.reason].
-				join(":");
-			lines.push(line);
-		}
+formatOutput = function (errors, filepath) {
+	var lines = [],
+        WARN = 'WARN',
+        ERR  = 'ERROR',
+	    i;
+	for (i = 0; i < errors.length; i += 1) {
+		var error = errors[i],
+            errorType = WARN,
+            nextError = i < errors.length ? errors[i + 1] : null;
+
+        if (error && error.reason && error.reason.match(/^Stopping/) === null) {
+            // If jslint stops next, this was an actual error
+            if (nextError && nextError.reason && nextError.reason.match(/^Stopping/) !== null) {
+                errorType = ERR;
+            }
+            lines.push([error.line, error.character, errorType, error.reason].join(":"));
+        }
 	}
 	return lines;
 };
 
 // generate an error (line, character, reason) from a warning (line, name)
-transformWarning = function(item, prefix) {
+transformWarning = function (item, prefix) {
 	return {
 		line: item.line,
 		character: 0,
@@ -156,15 +168,15 @@ transformWarning = function(item, prefix) {
 	};
 };
 
-parseOptions = function(args) {
+parseOptions = function (args) {
 	var opts = {};
 	var anon = [];
 	var i;
-	for(i = 0; i < args.length; i += 1) {
+	for (i = 0; i < args.length; i += 1) {
 		var arg = args[i];
-		if(arg.indexOf("--") === 0) {
+		if (arg.indexOf("--") === 0) {
 			arg = arg.substr(2);
-			if(arg.indexOf("=") === -1) {
+			if (arg.indexOf("=") === -1) {
 				opts[arg] = true;
 			} else {
 				var pair = arg.split("="); // NB: assumes exactly one "="
@@ -172,10 +184,14 @@ parseOptions = function(args) {
 				var value = pair[1];
 
 				// infer value type
-				if(value === "false") {
+				if (value === "false") {
 					value = false;
 				}
-				switch(name) { // XXX: special-casing JSLint-specifics
+				switch (name) { // XXX: special-casing JSLint-specifics
+                case "json-config":
+                    var c, data = fs.readFileSync(value, 'utf8'), config = JSON.parse(data);
+                    for (c in config) { if (config.hasOwnProperty(c)) { opts[c] = config[c]; } }
+                    break;
 				case "indent":
 				case "maxerr":
 				case "maxlen":
@@ -200,13 +216,13 @@ parseOptions = function(args) {
 	};
 };
 
-exit = function(status, msg) {
-	if(msg) {
+exit = function (status, msg) {
+	if (msg) {
 		process.stderr.write(msg + "\n");
 	}
 	try {
 		process.stdout.flush(); // required for legacy support (Node <0.5)?
-	} catch(exc) {}
+	} catch (exc) {}
 	process.exit(status ? 0 : 1);
 };
 
